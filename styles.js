22bfa421de6e8ff22f53fb4ae673f29d3c8b15b4
ratihhhhhhhhh <rtih705@gmail.com>
2025-01-08 22:43:43 -0800

    Create styles.js

---
+++ styles.js
@@ -0,0 +1,23 @@
+.product {
+    background: #fff;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    padding: 10px;
+    margin: 10px 0;
+    text-align: center;
+}
+
+.product img {
+    max-width: 100%;
+    height: auto;
+}
+
+footer {
+    text-align: center;
+    padding: 10px 0;
+    background: #333;
+    color: #fff;
+    position: relative;
+    bottom: 0;
+    width: 100%;
+}
